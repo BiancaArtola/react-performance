34bcdb5824581e3b72da0b75a9927e80f3383cf8
BiancaArtola <bian.artola@hotmail.com>
2022-06-28 14:05:48 -0300

    Complete exercise 1 extra 2

---
+++ src/exercise/01.extra-2.js
@@ -0,0 +1,40 @@
+// Code splitting
+// http://localhost:3000/isolated/exercise/01.js
+
+import * as React from 'react'
+
+const loadGlobe = () => import(/* webpackPrefetch: true*/'../globe')
+const Globe = React.lazy(loadGlobe)
+
+function App() {
+  const [showGlobe, setShowGlobe] = React.useState(false)
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        height: '100%',
+        padding: '2rem',
+      }}
+    >
+      <label style={{marginBottom: '1rem'}} onMouseOver={loadGlobe}>
+        <input
+          type="checkbox"
+          checked={showGlobe}
+          onChange={e => setShowGlobe(e.target.checked)}
+        />
+        {' show globe'}
+      </label>
+      <React.Suspense fallback="Loading..">
+        <div style={{width: 400, height: 400}}>
+          {showGlobe ? <Globe /> : null}
+        </div>
+      </React.Suspense>
+    </div>
+  )
+}
+
+export default App
